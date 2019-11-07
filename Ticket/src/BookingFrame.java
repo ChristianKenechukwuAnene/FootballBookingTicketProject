1e1fcafae164ec8d025003e52eb9a6dded5a216e
t00204290 <keecee_a@yahoo.com>
2019-11-07 16:00:30 +0000

    commit

--- Ticket/src/BookingFrame.java
+++ Ticket/src/BookingFrame.java
@@ -1,20 +1,34 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class  BookingFrame {
-    public static void main(String[] args ) {
+public class BookingFrame extends JFrame implements ActionListener{
+    // private instance variables
+    JButton b1;
 
-        //setting up jframe window
-        JFrame frame = new JFrame();
-        frame.setTitle("KEECEE TICKETING");
-        JLabel l1 = new JLabel(new ImageIcon("C:\\Users\\t00204290\\IdeaProjects\\FootballBookingTicketProject\\Ticket\\Apix.PNG"));
-        frame.add(l1,BorderLayout.CENTER);
 
-        JButton b1 = new JButton("Book Now");
-        frame.add(b1,BorderLayout.SOUTH);
+    public static void main (String [] args) {
+        BookingFrame Ticket = new BookingFrame();
+        Ticket.setVisible(true);
+    }
 
-        frame.setSize(500, 500);
-        frame.setVisible(true);
+            //setting up jframe window
+            public BookingFrame () {
+
+                setTitle("KEECEE TICKETING");
+                JLabel l1 = new JLabel(new ImageIcon("C:\\Users\\t00204290\\IdeaProjects\\FootballBookingTicketProject\\Ticket\\Apix.PNG"));
+                add(l1, BorderLayout.CENTER);
+
+                JButton b1 = new JButton("Book Now");
+                add(b1, BorderLayout.SOUTH);
+
+                setSize(500, 500);
+                setVisible(true);
+
+                b1.addActionListener(this);
+            }
+    public void actionPerformed(ActionEvent event) {
+        // add code to increment the buttonCount and display it on the button
 
     }
     }
